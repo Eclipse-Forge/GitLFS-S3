14d526ce6f27f03e2527653f37daf9e45981a593
Natsuya Kirishima <natsuya.kirishima@outlook.com>
2026-05-11 20:58:06 +1200

    Updated comments formatting
    Signed-off-by: Natsuya Kirishima <natsuya.kirishima@outlook.com>

--- server.js
+++ server.js
@@ -32,7 +32,7 @@
 //      ]
 //    }
 //
-// 3. Git LFS then directly talks to S3/B2 using the signed URLs.
+// 3. Git LFS then directly talks to S3/B2 using the signed URLs, this server is stateless and only signs requests.
 
 import express from 'express';
 import aws4 from 'aws4';
